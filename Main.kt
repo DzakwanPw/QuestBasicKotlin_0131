78197a2be86530f41e4e726e89a6150ec3faaae6
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-09-24 20:28:51 +0700

    Menambahkan Function Variable

--- Main.kt
+++ Main.kt
@@ -2,6 +2,14 @@ fun helloWorld() {
     println("Hello, world!")
 }
 
+fun variables() {
+    val customers = 10
+    var nama = "Budi"
+    nama = "Andi"
+    println("There are $customers customers, nama: $nama")
+}
+
 fun main() {
     helloWorld()
+    variables()
 }
