022285fc36a1c8c7cae396dbb44fb809a54a9cbf
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-09-24 20:31:46 +0700

    Menambahkan Function Collection

--- Main.kt
+++ Main.kt
@@ -9,7 +9,18 @@ fun variables() {
     println("There are $customers customers, nama: $nama")
 }
 
+fun collection() {
+    val shapes = mutableListOf("triangle", "square", "circle")
+    shapes.add("star")
+    println(shapes)
+    val fruit = setOf("apple", "banana", "cherry", "cherry")
+    println(fruit)
+    val juice = mapOf("apple" to 100, "kiwi" to 190)
+    println("Harga apple: ${juice["apple"]}")
+}
+
 fun main() {
     helloWorld()
     variables()
+    collection()
 }
